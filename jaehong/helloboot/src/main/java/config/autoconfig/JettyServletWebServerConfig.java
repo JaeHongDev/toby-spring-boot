d31d86ef7eecf6c6dffbd1c73764e7977a08beb5
JaehongDev <println@kakao.com>
2023-04-22 16:28:27 +0900

    feature:자동 구성 정보 대체하기

--- jaehong/helloboot/src/main/java/config/autoconfig/JettyServletWebServerConfig.java
+++ jaehong/helloboot/src/main/java/config/autoconfig/JettyServletWebServerConfig.java
@@ -3,6 +3,7 @@ package config.autoconfig;
 
 import config.ConditionalMyOnClass;
 import config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Bean;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyServletWebServerConfig {
-    @Bean
+    @Bean("JettyServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
--- jaehong/helloboot/src/main/java/config/autoconfig/TomcatServletWebServerConfig.java
+++ jaehong/helloboot/src/main/java/config/autoconfig/TomcatServletWebServerConfig.java
@@ -2,6 +2,7 @@ package config.autoconfig;
 
 import config.ConditionalMyOnClass;
 import config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +10,8 @@ import org.springframework.context.annotation.Bean;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatServletWebServerConfig {
-    @Bean
+    @Bean("TomcatServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
