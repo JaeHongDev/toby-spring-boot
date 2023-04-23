12ea8cff736de3bfd5d3b90e8c22d46b4cb69823
JaehongDev <println@kakao.com>
2023-04-23 16:35:24 +0900

    feature:@Value와 PropertySourcesPlaceholderConfigurer

--- jaehong/helloboot/src/main/java/config/autoconfig/TomcatServletWebServerConfig.java
+++ jaehong/helloboot/src/main/java/config/autoconfig/TomcatServletWebServerConfig.java
@@ -2,6 +2,7 @@ package config.autoconfig;
 
 import config.ConditionalMyOnClass;
 import config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -10,9 +11,15 @@ import org.springframework.context.annotation.Bean;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatServletWebServerConfig {
+
+    @Value("${context.path}")
+    private String contextPath;
+
     @Bean("TomcatServerFactory")
     @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
+
+        System.out.println(contextPath);
         return new TomcatServletWebServerFactory();
     }
 }
